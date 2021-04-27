963d855398f77e6eb4e58e83fd381663dcbe66ba
Nayana-01 <81194632+Nayana-01@users.noreply.github.com>
2021-04-27 18:48:29 -0400

    Add files via upload

---
+++ c27/AngryBirdsStage2.5StudentActivity/slingShot.js
@@ -0,0 +1,23 @@
+class SlingShot{
+    constructor(bodyA, bodyB){
+       var option = {
+        bodyA: bodyA,
+        bodyB: bodyB,
+        stiffness:0.04,
+        length:10
+       }
+this.body=Constraint.create(option)
+World.add(world, this.body);
+
+    }
+    display(){
+        var pointA=this.body.bodyA.position;
+        var pointB = this.body.bodyB.position;
+        strokeWeight(3)
+        line(pointA.x, pointA.y, pointB.x, pointB.y)
+        
+
+    }
+
+
+}
